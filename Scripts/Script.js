760678d3431d60c1e2253798362dfff3b57d0d80
boliverse <112529539+boliverse@users.noreply.github.com>
2025-05-14 01:13:00 -0500

    Update Script.js

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,6 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+
   if (req.method === 'GET' && req.url === '/api/data') {
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = 200;
